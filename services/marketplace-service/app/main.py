b2f9407880145cf8c6ca50ad16daef74cbf9c272
Preethi <preethikgowda26@gmail.com>
2026-04-23 18:38:05 +0530

    Fix CORS rules and point frontend to EC2 IP

--- services/marketplace-service/app/main.py
+++ services/marketplace-service/app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://54.87.19.205:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- services/user-service/app/main.py
+++ services/user-service/app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://54.87.19.205:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
